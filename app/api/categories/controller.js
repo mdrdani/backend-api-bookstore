dfe59ba2cd3f8516a1a5070b9da241d241a45fd5
mdrdani <sudokill3004@example.com>
2024-03-09 16:36:31 +0700

    CRUD Categories

---
+++ app/api/categories/controller.js
@@ -0,0 +1,73 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prismaClient = new PrismaClient();
+
+module.exports = {
+  getAllCategories: async (req, res, next) => {
+    try {
+      const categories = await prismaClient.category.findMany({
+        where: {
+          userId: req.user.id,
+        },
+      });
+      res
+        .status(200)
+        .json({ message: 'Success get all categories', data: categories });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  createCategories: async (req, res, next) => {
+    try {
+      const { name } = req.body;
+      const categories = await prismaClient.category.create({
+        data: {
+          name: name,
+          userId: req.user.id,
+        },
+      });
+      res
+        .status(200)
+        .json({ message: 'Success create category', data: categories });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  updateCategories: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const categories = await prismaClient.category.update({
+        where: {
+          id: parseInt(id),
+        },
+        data: {
+          name: name,
+        },
+      });
+      res
+        .status(200)
+        .json({ message: 'Success update category', data: categories });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  deleteCategories: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const categories = await prismaClient.category.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      res
+        .status(200)
+        .json({ message: 'Success delete category', data: categories });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../../middleware/auth.js');
+const {
+  getAllCategories,
+  createCategories,
+  updateCategories,
+  deleteCategories,
+} = require('./controller.js');
 
 /* GET home page. */
-router.get('/categories', auth, function (req, res) {
-  res.status(200).json({ message: 'Categories route' });
-});
+router.get('/categories', auth, getAllCategories);
+router.post('/categories', auth, createCategories);
+router.put('/categories/:id', auth, updateCategories);
+router.delete('/categories/:id', auth, deleteCategories);
 
 module.exports = router;
