006e26729d75865c6fdcb8868bee645fddbbc861
mdrdani <sudokill3004@example.com>
2024-03-09 16:00:18 +0700

    signup

--- app/api/auth/controller.js
+++ app/api/auth/controller.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const prismaClient = new PrismaClient();
 
 module.exports = {
+  // signin
   signin: async (req, res, next) => {
     try {
       const { email, password } = req.body;
@@ -40,4 +41,38 @@ module.exports = {
       next(err);
     }
   },
+  // end
+
+  // signup
+  signup: async (req, res, next) => {
+    try {
+      const { name, email, password, confirmPassword } = req.body;
+      if (password !== confirmPassword) {
+        res.status(403).json({ message: 'Password not match' });
+      }
+
+      const checkEmail = await prismaClient.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      if (checkEmail) {
+        return res.status(403).json({ message: 'Email already exists' });
+      }
+
+      const user = await prismaClient.user.create({
+        data: {
+          name,
+          email,
+          password: bcrypt.hashSync(password, 10),
+          role: 'Admin',
+        },
+      });
+      delete user.password;
+      res.status(200).json({ message: 'Success Signup', data: user });
+    } catch (err) {
+      next(err);
+    }
+  },
+  // end
 };
--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('../../middleware/auth.js');
 
 /* GET home page. */
-router.get('/categories', function (req, res) {
+router.get('/categories', auth, function (req, res) {
   res.status(200).json({ message: 'Categories route' });
 });
 
