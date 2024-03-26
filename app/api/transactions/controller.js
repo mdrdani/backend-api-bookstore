41e09144f70e73001df2077940caeb9dd8358f1f
mdrdani <sudokill3004@example.com>
2024-03-26 13:23:23 +0700

    detail transaction list

--- app/api/transactions/controller.js
+++ app/api/transactions/controller.js
@@ -31,11 +31,33 @@ module.exports = {
             transactionDetail: true,
           },
         });
-        console.log(transaction);
         return res
           .status(200)
           .json({ message: 'Success get all transactions', data: transaction });
       }
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  detailTransactionsList: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const detailTransaction = await prismaClient.transaction.findUnique({
+        where: {
+          id: parseInt(id),
+          userId: req.user.id,
+        },
+        include: {
+          transactionDetail: true,
+        },
+      });
+
+      return res.status(200).json({
+        message: 'Success get detail transactions',
+        data: detailTransaction,
+      });
     } catch (err) {
       next(err);
     }
--- app/api/transactions/router.js
+++ app/api/transactions/router.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../../middleware/auth.js');
-const { getTransactionsList } = require('./controller.js');
+const {
+  getTransactionsList,
+  detailTransactionsList,
+} = require('./controller.js');
 
 /* GET home page. */
 router.get('/transactions', auth, getTransactionsList);
+router.get('/transactions/:id', auth, detailTransactionsList);
 
 module.exports = router;
